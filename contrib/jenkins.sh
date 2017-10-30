f51afec8fa8b54b998e245696528b5a336940cad
Neels Hofmeyr <neels@hofmeyr.de>
2017-10-30 05:00:55 +0100

    jenkins: use common osmo-build-dep.sh and osmo-clean-workspace.sh
    Since the gerrit build jobs no longer contain git clean workspace config (for
    good reasons), it is important to use osmo-clean-workspace.sh. To make it work
    best, this jenkins.sh should follow the same structure as most others do.
    
    Change-Id: I3eca957c52b2c018e4c784b29330a0d06c4e3595

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,15 +1,40 @@
-#!/bin/sh -ex
+#!/usr/bin/env bash
+# jenkins build helper script for openbsc.  This is how we build on jenkins.osmocom.org
 
-rm -rf deps/install
-mkdir deps || true
-cd deps
-osmo-deps.sh libosmocore
+if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
+	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
+	exit 2
+fi
 
-cd libosmocore
-autoreconf --install --force
-./configure --prefix=$PWD/../install
-$MAKE $PARALLEL_MAKE install
+set -ex
 
-cd ../../src
-make clean || true
-PKG_CONFIG_PATH=$PWD/../deps/install/lib/pkgconfig $MAKE
+base="$PWD"
+deps="$base/deps"
+inst="$deps/install"
+export deps inst
+
+osmo-clean-workspace.sh
+
+mkdir "$deps" || true
+
+osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
+
+verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
+
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+
+set +x
+echo
+echo
+echo
+echo " =============================== osmo-tetra ==============================="
+echo
+set -x
+
+cd "$base"
+cd src
+$MAKE clean || true
+$MAKE
+
+osmo-clean-workspace.sh
